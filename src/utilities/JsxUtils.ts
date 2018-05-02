63e5a65b23b2ccd2d583e3984b2166f36147713b
Steven.Udall <svus55@gmail.com>
2018-05-01 18:19:53 -0700

    more JsxUtils ideas

--- src/utilities/JsxUtils.ts
+++ src/utilities/JsxUtils.ts
@@ -1,10 +1,20 @@
 class JsxUtils {
-    static if<TResult, TElseResult>(condition: boolean, thenResult: TResult, elseResult: TElseResult | null = null) {
+    static if<TThenResult, TElseResult>(condition: boolean, thenResult: TThenResult, elseResult: TElseResult | null = null) {
         if (condition) {
             return thenResult;
         } else {
             return elseResult;
         }
+    }
+
+    static forCount<TResult>(count: number, operation: (index: number) => TResult): TResult[] {
+        const result: TResult[] = [];
+
+        for (let index = 0; index < count; index++) {
+            result.push(operation(index));
+        }
+
+        return result;
     }
 
     static for<TResult>(endCondition: () => boolean,
@@ -21,24 +31,46 @@ class JsxUtils {
         return result;
     }
 
-    static forCount<TResult>(count: number, operation: (index: number) => TResult): TResult[] {
-        const result: TResult[] = [];
+    static switchIf<TResult>(cases: {condition: boolean,
+        result: TResult | (() => TResult)}[],
+        defaultResult: TResult | null = null) {
 
-        for (let index = 0; index < count; index++) {
-            result.push(operation(index));
+        const trueCase = cases.find((switchCase) => {
+            return switchCase.condition;
+        });
+
+        if (trueCase != null) {
+            if (typeof trueCase.result === "function") {
+                return trueCase.result();
+            }
+
+            return trueCase.result;
         }
 
-        return result;
+        return null;
     }
 
-    static ifElseIf(conditionAndResults: {condition: boolean, result: any}[]) {
-        conditionAndResults.forEach((conditionAndResult) => {
-            if (conditionAndResult.condition) {
-                return conditionAndResult.result;
+    static switch<TSwitchValue, TResult>(switchValue: TSwitchValue,
+        cases: {value: TSwitchValue, result: TResult | ((value: TSwitchValue) => TResult)}[],
+        defaultResult: TResult | null = null) {
+
+        const newCases = cases.map((switchCase) => {
+            let newResult: TResult | (() => TResult) = switchCase.result as TResult;
+
+            if (typeof switchCase.result === "function") {
+                newResult = () => {
+                    const resultFunction = switchCase.result as ((value: TSwitchValue) => TResult);
+                    return resultFunction(switchCase.value);
+                };
+            }
+
+            return {
+                condition: switchCase.value === switchValue,
+                result: newResult
             }
         });
 
-        return null;
+        return this.switchIf(newCases, defaultResult);
     }
 }
 
